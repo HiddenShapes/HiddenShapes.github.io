13a9d4c4cdb7b5e042c8a05f8f87feb99fb41db1
leonard <leonardebay@gmail.com>
2023-05-01 12:04:32 +0200

    clément comments

--- newton-method/newt2.py
+++ newton-method/newt2.py
@@ -1,15 +1,15 @@
 def f(x):
     return x**3 - 3*x**2 - 1
 
-def fprime(x):
+def df(x):
     return 3*x**2 - 6*x
 
 x0 = 2
-tol = 1e-6
+tol = 1e-10
 
 while True:
   # Appliquer la fonction d'itération
-  x1 = x0 - f(x0)/fprime(x0);
+  x1 = x0 - f(x0) / df(x0);
   
   # Vérifier si la distance entre les approximations est inférieure à la tolérance
   if abs(x1 - x0) < tol:
